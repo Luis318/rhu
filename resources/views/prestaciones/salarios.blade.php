60bb6b136ea02535e73e0d0b3651de919baf5557
Luis318 <mg17032@ues.edu.sv>
2023-05-06 14:42:19 -0600

    datatable v1

--- resources/views/prestaciones/salarios.blade.php
+++ resources/views/prestaciones/salarios.blade.php
@@ -7,40 +7,60 @@
                 <div class="row justify-content-center">
 
                     <div class="container d-flex justify-content-end mb-3">
-                        <button type="button" name="create" id="create" class="btn btn-success"><i class="bi bi-plus-square"></i></button>
+                        <button type="button" name="create" id="create" class="btn btn-success"><i
+                                class="bi bi-plus-square"></i></button>
                     </div>
+                    <div class="container-xl col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <table class="table table-striped table-bordered" id="emp_table">
+                                    <thead>
+                                        <th scope="col">Nombre</th>
+                                        <th scope="col">Apellido</th>
+                                        <th scope="col">DUI</th>
+                                        <th scope="col">Salario</th>
+                                        <th scope="col">Acciones</th>
+                                    </thead>
 
-                    <table class="table table-striped table-bordered" id="emp_table">
-                        <thead>
-                            <th>Nombre</th>
-                            <th>Apellido</th>
-                            <th>DUI</th>
-                            <th>Salario</th>
-                            <th>Acciones</th>
-                        </thead>
-                       
-                    </table>
-
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
     </div>
 
     <script>
-    $(document).ready(function() {
-        var tabla = new DataTable('#emp_table',{
-            processing: true,
-            serverSide: true,
-            ajax: "{{ route('salarios') }}",
-            columns: [
-                {data: 'primerNombre', name: 'Nombre'},
-                {data: 'primerApellido', name: 'Apellido'},
-                {data: 'dui', name: 'DUI'},
-                {data: 'salario_base', name: 'Salario'},
-                {data: 'acciones', orderable: false, searchable: false},
-            ]
+        $(document).ready(function() {
+            var tabla = new DataTable('#emp_table', {
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('salarios') }}",
+                columns: [{
+                        data: 'primerNombre',
+                        name: 'Nombre'
+                    },
+                    {
+                        data: 'primerApellido',
+                        name: 'Apellido'
+                    },
+                    {
+                        data: 'dui',
+                        name: 'DUI'
+                    },
+                    {
+                        data: 'salario_base',
+                        name: 'Salario'
+                    },
+                    {
+                        data: 'acciones',
+                        orderable: false,
+                        searchable: false
+                    },
+                ]
+            });
+            console.log(tabla);
         });
-    });
-
     </script>
 @endsection
